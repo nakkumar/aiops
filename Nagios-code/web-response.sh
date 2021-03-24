39c097888424ac3eee5fc8e6c0892dd61566b455
arun <61176789+nakkumar@users.noreply.github.com>
2021-03-24 19:08:40 +0530

    Update web-response.sh

--- Nagios-code/web-response.sh
+++ Nagios-code/web-response.sh
@@ -20,6 +20,27 @@ else
 
 fi
 
+############# ubuntu  ####################################
+
+#!/bin/bash
+val=$(curl -s -w %{time_total}\\n -o /dev/null http://192.168.4.244/demo.php)
+min=0.111
+if (( ${val%%.*} < ${min%%.*} || ( ${val%%.*} == ${min%%.*} && ${val##*.} < ${min##*.} ) )) ; then
+                echo "OK - Usage of   $val  services in okay  state"
+                exit 0
+
+else
+                echo "CRITICAL - Usage of  $val services in Critical state"
+                curl -D - \
+                -X "POST" -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "X-Rundeck-Auth-Token: NSUt9PpUTTJ6INH7xcjt09VyMcr8cexX" \
+                -d "{\"argString\":\"-servicename $URL \"}" \
+                http://192.168.5.116:4440/api/16/job/0a9cd796-9d84-4771-b811-a1cdaaf37400/executions
+                exit 2
+
+fi
+
 
 ###############################################################################################################################
 
