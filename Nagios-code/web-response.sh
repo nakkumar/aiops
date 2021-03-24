e2e04ec16c42b96e909db5c7fe2c76908793ee1b
arun <61176789+nakkumar@users.noreply.github.com>
2021-03-24 18:47:39 +0530

    Update web-response.sh

--- Nagios-code/web-response.sh
+++ Nagios-code/web-response.sh
@@ -1,3 +1,31 @@
+########### centos ##############################
+
+
+#!/bin/bash
+val=$(curl -s -w %{time_total}\\n -o /dev/null http://192.168.4.115/demo.php)
+min=0.111
+if (( ${val%%.*} < ${min%%.*} || ( ${val%%.*} == ${min%%.*} && ${val##*.} < ${min##*.} ) )) ; then
+                echo "OK - Usage of   $val  services in okay  state"
+                exit 0
+
+else
+                echo "CRITICAL - Usage of  $val  services in Critical state"
+                curl -D - \
+                -X "POST" -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "X-Rundeck-Auth-Token: NSUt9PpUTTJ6INH7xcjt09VyMcr8cexX" \
+                -d "{\"argString\":\"-servicename $URL \"}" \
+                http://192.168.5.116:4440/api/16/job/6fca6546-7d48-4c3f-be0d-977c594c2f0a/executions
+                exit 2
+
+fi
+
+
+###############################################################################################################################
+
+
+
+
 #!/bin/bash
 val=$(curl -s -w %{time_total}\\n -o /dev/null http://192.168.4.115/demo.php)
 min=0.111
