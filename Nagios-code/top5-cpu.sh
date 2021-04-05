1579a43382acc275a5d0045c960e0c11bca421d0
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-05 21:08:05 +0530

    Update top5-cpu.sh

--- Nagios-code/top5-cpu.sh
+++ Nagios-code/top5-cpu.sh
@@ -80,4 +80,30 @@ fi
 
 Windows
 *******
+$total=(Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
+$test = Get-Process -IncludeUserName | Sort-Object CPU -desc | Select -first 6 | Select ProcessName | Format-Table -AutoSize | out-file -filepath C:\top5cpu_data.txt
+$test2 = Get-Content "C:\top5cpu_data.txt" | Select-Object -last 7
+$test3 = $test2 -replace '\s',''
+$name = $test3 | Select-Object -first 1
 
+#$name = $test3 | Select-Object -first 1
+
+#Write-Output("$test3")
+
+if($total -ge 0 -and $total -le 60){
+   Write-Output("OK - Usage of top5 cpu: '$test3 --> $name'  process OK State")
+   exit 0
+} elseif($total -ge 71 -and $total -le 80){
+  C:\Python\Python37\python.exe C:\Users\cpu_utilization.py
+   Write-Output("WARNING - Usage of top5 cpu: '$test3 --> $name'  process in WARNING State")
+   exit 1
+}
+ elseif($total -ge 81 ){
+   C:\Python\Python37\python.exe C:\Users\cpu_utilization.py
+   Write-Output("CRITICAL - Usage of top5 cpu: '$test3 --> $name'  process in CRITICAL State")
+   exit 2
+}
+ else{
+   Write-Output("UNKNOWN - Usage of top5 cpu: '$test3 --> $name'  process in UNKNOWN State")
+   exit 3
+}
