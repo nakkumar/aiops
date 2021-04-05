7fefa3d0865d6b3ddbe634d64bce59847af3e18f
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-05 12:11:07 +0530

    Update top5-cpu.sh

--- Nagios-code/top5-cpu.sh
+++ Nagios-code/top5-cpu.sh
@@ -28,3 +28,30 @@ fi
 
 ubuntu
 ******
+#!/bin/bash
+
+countWarnings=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk  '{ print $2 }')
+
+top=$(ps -Ao comm --sort=-pcpu | head -n 6 | tail -5 | awk  'BEGIN { ORS="," } { print }')
+
+if (( ${countWarnings%%.*}<=30 )); then
+                echo "OK - CPU Usage of top5 process : $top"
+                exit 0
+
+        elif (( 31<=${countWarnings%%.*} && ${countWarnings%%.*}<=40 )); then
+                echo "WARNING - CPU Usage top5 process : $top "
+                exit 1
+
+        elif (( 41<=${countWarnings%%.*} && ${countWarnings%%.*}<=100 )); then
+                echo "CRITICAL - CPU Usage top5 process : $top "
+                exit 2
+        else
+                echo "UNKNOWN - $top"
+                exit 3
+fi
+
+#######################################################################################################
+
+Windows
+*******
+
