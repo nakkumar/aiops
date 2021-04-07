435c705cb3914efddcef5db854d1a3f6a7ade543
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-07 12:39:12 +0530

    Update top5-cpu.sh

--- Nagios-code/top5-cpu.sh
+++ Nagios-code/top5-cpu.sh
@@ -1,6 +1,47 @@
 centos
 *******
 
+
+#!/bin/bash
+
+countWarnings=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk  '{ print $2 }')
+
+top=$(ps -Ao comm --sort=-pcpu | head -n 6 | tail -5 | awk  'BEGIN { ORS="," } { print }')
+
+pid=$(ps -Ao pid --sort=-pcpu | head -n 6 | awk -F: 'NR==2 {print $1}')
+
+if (( ${countWarnings%%.*}<=30 )); then
+                echo "OK - CPU Usage of top5 process : $top --> $pid"
+                 curl -D - \
+                -X "POST" -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "X-Rundeck-Auth-Token: WApjt25lt0SN8ONS0nRJXc7mqrZ8lQzC" \
+                -d "{\"argString\":\"-pid $pid  \"}" \
+#                http://192.168.5.116:4440/api/16/job/e5c5e06e-9fa7-4d5d-af12-280df85eaa6d/executions
+
+                exit 0
+
+        elif (( 31<=${countWarnings%%.*} && ${countWarnings%%.*}<=40 )); then
+                echo "WARNING - CPU Usage top5 process : $top --> $pid "
+                curl -D - \
+                -X "POST" -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "X-Rundeck-Auth-Token: WApjt25lt0SN8ONS0nRJXc7mqrZ8lQzC" \
+                -d "{\"argString\":\"-pid $pid  \"}" \
+#                http://192.168.5.116:4440/api/16/job/e5c5e06e-9fa7-4d5d-af12-280df85eaa6d/executions
+                exit 1
+
+        elif (( 41<=${countWarnings%%.*} && ${countWarnings%%.*}<=100 )); then
+                echo "CRITICAL - CPU Usage top5 process : $top --> $pid "
+                exit 2
+        else
+                echo "UNKNOWN - $top --> $pid"
+                exit 3
+fi
+
+
+*************************************************************************************************
+
 #!/bin/bash
 
 countWarnings=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk  '{ print $2 }')
@@ -54,6 +95,47 @@ fi
 
 ubuntu
 ******
+
+#!/bin/bash
+
+countWarnings=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk  '{ print $2 }')
+
+top=$(ps -Ao comm --sort=-pcpu | head -n 6 | tail -5 | awk  'BEGIN { ORS="," } { print }')
+
+pid=$(ps -Ao pid --sort=-pcpu | head -n 6 | awk -F: 'NR==2 {print $1}')
+
+if (( ${countWarnings%%.*}<=30 )); then
+                echo "OK - CPU Usage of top5 process : $top --> $pid"
+                 curl -D - \
+                -X "POST" -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "X-Rundeck-Auth-Token: WApjt25lt0SN8ONS0nRJXc7mqrZ8lQzC" \
+                -d "{\"argString\":\"-pid $pid  \"}" \
+#                http://192.168.5.116:4440/api/16/job/ea1261e3-2981-43ed-a4b9-2e07b7e06bfe/executions
+
+                exit 0
+
+        elif (( 31<=${countWarnings%%.*} && ${countWarnings%%.*}<=40 )); then
+                echo "WARNING - CPU Usage top5 process : $top --> $pid "
+                curl -D - \
+                -X "POST" -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "X-Rundeck-Auth-Token: WApjt25lt0SN8ONS0nRJXc7mqrZ8lQzC" \
+                -d "{\"argString\":\"-pid $pid  \"}" \
+#                http://192.168.5.116:4440/api/16/job/ea1261e3-2981-43ed-a4b9-2e07b7e06bfe/executions
+                exit 1
+
+        elif (( 41<=${countWarnings%%.*} && ${countWarnings%%.*}<=100 )); then
+                echo "CRITICAL - CPU Usage top5 process : $top --> $pid "
+                exit 2
+        else
+                echo "UNKNOWN - $top --> $pid"
+                exit 3
+fi
+
+
+*******************************************************************
+
 #!/bin/bash
 
 countWarnings=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk  '{ print $2 }')
