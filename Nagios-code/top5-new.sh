0b35bc41d6256c49da498462ad118bdb2753d0a7
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-08 20:00:29 +0530

    Update top5-new.sh

--- Nagios-code/top5-new.sh
+++ Nagios-code/top5-new.sh
@@ -112,3 +112,57 @@ while($val -le $count1)
 	  
 	  #Write-Output "$value3"
      }
+     
+ 
+ #########################
+ 
+ $test = Get-Process | Sort-object CPU -Descending | Select -First 6 -Property ProcessName | out-file -filepath C:\top5cpu_data2.txt
+$test2 = Get-Content "C:\top5cpu_data.txt" | Select-Object -last 7
+#$test3 = $test2 -replace '\s',''
+# Get-Process | Sort-object CPU -Descending | Select -First 6 -Property ProcessName,cpu,id
+#$name = $test3 | Select-Object -first 1
+Write-Output("******************** $test2")
+
+$pid1 = Get-Process | Sort-object CPU -Descending | Select -First 2 -Property id | Select -last 1 | out-file -filepath C:\top5cpu_id1.txt
+$pid2 = Get-Content "C:\top5cpu_id1.txt" | Select-Object -last 3
+#Write-Output("******************** $pid2")
+
+#taskkill /PID $pid2 /F
+
+################ service restart #######################
+
+$value=get-service | select-object Name | out-file -filepath C:\topcpu.txt
+
+$count=(get-service | select-object Name).Length
+
+$count1=$count+1
+
+$val=2
+
+while($val -le $count1)
+     {
+      $val++
+      Write-Output  "$val"
+
+          $value2=Get-Content "C:\topcpu.txt" | Select-Object -Index $val
+
+          $space=$value2 -replace '\s',''
+
+          #Write-Output "$value2"
+
+          $value3=tasklist /svc /fi "SERVICES eq $space" | select-string '$pid2'
+
+          if($value3)
+          {
+                  Write-Output "service#############$space#######restared"
+                  Restart-Service  $space
+
+          }
+          else
+          {
+                  Write-Output "no valuse"
+          }
+
+          #Write-Output "$value3"
+     }
+
