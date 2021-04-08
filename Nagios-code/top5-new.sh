d1c7c2fa88d47aa7a60dc9fc6ad01798c0dffbef
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-08 13:34:03 +0530

    Update top5-new.sh

--- Nagios-code/top5-new.sh
+++ Nagios-code/top5-new.sh
@@ -27,3 +27,49 @@ echo "$test"
 #    sleep 2
 
 done
+
+#########################################################################################################
+
+#!/bin/bash
+
+#systemctl list-units --type=service | awk '{ print $1 }' | grep '.service'  > /home/centos/top5/data.txt
+
+count=$(systemctl list-units --type=service | awk '{ print $1 }' | grep '.service' | wc -l)
+
+echo "$count"
+
+max=1
+while [ $max -le $count ]
+do
+    echo $max
+
+val=$(systemctl list-units --type=service | awk '{ print $1 }' | grep '.service' | awk -F: 'NR=='$max' {print $1}')
+
+    echo "$val"
+
+    max=$(($max+1))
+
+test=$(systemctl status $val | grep '24118'| awk '{ print $1 }' | awk -F: 'NR=='1' { print $1 }')
+
+
+if [ ! -z $test ]; then
+
+echo "ok"
+
+systemctl restart $val
+
+echo "service $val restarted"
+
+
+else
+
+echo "###########novalue#############"
+
+fi
+
+#echo "$test"
+
+
+    sleep 0.5
+
+done
