6b61d972170f8ba252dea1e44b09e50ca041c9d4
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-15 16:47:43 +0530

    Update top5-memory.sh

--- Nagios-code/top5-memory.sh
+++ Nagios-code/top5-memory.sh
@@ -1,3 +1,34 @@
+Node-script
+***********
+
+#!/bin/bash
+
+countWarnings=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }' | sed 's/%//g')
+name=$(ps -eo comm --sort=-%mem | head -n 6 | tail -5 | awk  'BEGIN { ORS="," } { print }')
+
+if (( ${countWarnings%%.*}<=60 )); then
+                echo "OK - Usage of   ${countWarnings%%.*} % top 5 process --> $name "
+                exit 0
+        elif (( 61<=${countWarnings%%.*} && ${countWarnings%%.*}<=70 )); then
+                echo "WARNING - Usage of  ${countWarnings%%.*} % top 5 process --> $name"
+                exit 1
+        elif (( 71<=${countWarnings%%.*} && ${countWarnings%%.*}<=100 )); then
+                echo "CRITICAL - Usage of  ${countWarnings%%.*} % top 5 process --> $name"
+#                curl -D - \
+#                -X "POST" -H "Accept: application/json" \
+#                -H "Content-Type: application/json" \
+#                -H "X-Rundeck-Auth-Token: BRaq7hklM9OxbHL5geLS8hG6AkJASbed" \
+#                http://192.168.5.116:4440/api/16/job/6132d92c-423c-4e85-852a-df1edf5a96eb/executions
+                exit 2
+        else
+                echo "UNKNOWN - ${countWarnings%%.*}"
+                exit 3
+fi
+
+
+Rundeck-script
+***************
+
 #!/bin/bash
 
 ########variable($idd)#########idd=$(ps -eo pid --sort=-%mem | head -n 6 | awk -F: 'NR==2 {print $1}')
