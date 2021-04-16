74570ee188fab8c713d20ab382a4e3a5597496ab
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-16 17:46:28 +0530

    Create top5-io-windows.ps1

---
+++ Nagios-code/top5-io-windows.ps1
@@ -0,0 +1,35 @@
+### print the top 5 io process ###
+
+gwmi Win32_PerfFormattedData_PerfProc_Process | sort IOReadBytesPersec -des | select  Name -First 6 | out-file -filepath C:\top5io_name.txt
+
+$name = Get-Content "C:\top5io_name.txt" | Select-Object -last 7 | where{$_ -ne ""}
+
+$name2 = $name -replace '\s',''
+#Write-Output("$name2")
+
+### calculate the io usage of the system ###
+
+$Counter = Get-Counter -Counter "\LogicalDisk(*)\Avg. Disk sec/Write"
+
+$Counter.CounterSamples[4] |  select-object CookedValue | out-file -filepath C:\io_data.txt
+
+$total = Get-Content "C:\io_data.txt" | Select-Object -Index 3 | where{$_ -ne ""}
+#Write-Output "$total"
+
+if($total -le 60){
+   Write-Output("ok - Usage of $total top5 io process --> $name2")
+   exit 0
+} elseif($total -ge 61 -and $total -le 70){
+
+   Write-Output("WARNING - Usage of $total top5 io process --> $name2")
+   exit 1
+}
+ elseif($total -ge 71 ){
+
+   Write-Output("CRITICAL - Usage of $total top5 io process --> $name2")
+   exit 2
+}
+ else{
+   Write-Output("UNKNOWN - Usage of $total top5 io process --> $name2")
+   exit 3
+}
