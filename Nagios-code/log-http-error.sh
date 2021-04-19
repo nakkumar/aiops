b95b2bb0afe8c744b3e44249a7e1488497405d01
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-19 18:50:04 +0530

    v1.1

--- Nagios-code/log-http-error.sh
+++ Nagios-code/log-http-error.sh
@@ -1,3 +1,33 @@
+centos and ubuntu log file name and path diff
+**********************************************
+
+centos
+******
+#!/bin/bash
+
+msg=$(tail -n 1 /var/log/httpd/error_log | grep -w down)
+
+down=$(tail -n 1 /var/log/httpd/error_log | grep -w down | wc -l)
+
+#echo "$down"
+
+if (( $down>=1 ));then
+
+        {
+
+        echo "WARNING $msg "
+        exit 1
+        }
+else
+        {
+        echo "ok "
+        exit 0
+        }
+fi
+
+
+
+
 ubuntu
 ******
 
