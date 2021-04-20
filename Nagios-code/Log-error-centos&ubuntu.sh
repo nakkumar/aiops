7887d3cbadebd47b4c5336576681350cfeb52c51
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-20 11:12:17 +0530

    v1.0

---
+++ Nagios-code/Log-error-centos&ubuntu.sh
@@ -0,0 +1,26 @@
+Ubuntu
+*******
+## path -->  /var/log/syslog
+#!/bin/bash
+
+# mkdir /home/$(whoami)/log
+ touch /home/$(whoami)/log/data_after1
+ touch /home/$(whoami)/log/data_before1
+
+ tail -1 /var/log/syslog > /home/$(whoami)/log/data_after1
+ msg=$(tail -1 /home/$(whoami)/log/data_after1)
+ if cmp -s /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
+ then
+         {
+             echo "ok --> NO new logs"
+             exit 0
+     }
+else
+        {
+             echo "WARNING --> $msg"
+             cp /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
+             exit 1
+        }
+
+
+ fi
