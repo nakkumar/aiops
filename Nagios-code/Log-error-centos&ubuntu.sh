6e876db12cf068dc21aa21c4d60e9ad99c3528b5
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-20 11:52:39 +0530

    v1.2

--- Nagios-code/Log-error-centos&ubuntu.sh
+++ Nagios-code/Log-error-centos&ubuntu.sh
@@ -7,7 +7,7 @@ Ubuntu
  touch /home/$(whoami)/log/data_after1
  touch /home/$(whoami)/log/data_before1
 
- tail -1 /var/log/syslog > /home/$(whoami)/log/data_after1
+sudo tail -1 /var/log/syslog > /home/$(whoami)/log/data_after1
  msg=$(tail -1 /home/$(whoami)/log/data_after1)
  if cmp -s /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
  then
@@ -20,6 +20,35 @@ else
              echo "WARNING --> $msg"
              cp /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
              exit 1
+        }
+
+
+ fi
+
+Centos
+******
+
+#!/bin/bash
+
+# mkdir /home/$(whoami)/log
+ touch /home/$(whoami)/log/data_after1
+ touch /home/$(whoami)/log/data_before1
+
+sudo  tail -1 /var/log/secure > /home/$(whoami)/log/data_after1
+ msg=$( tail -1 /home/$(whoami)/log/data_after1)
+ if cmp -s /home/$(whoami)/log/data_after1  /home/$(whoami)/log/data_before1
+ then
+         {
+            echo "ok --> NO new logs"
+            exit 0
+
+     }
+else
+        {
+             echo "WARNING --> $msg"
+             cp /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
+             exit 1
+
         }
 
 
