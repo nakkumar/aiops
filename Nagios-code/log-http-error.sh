6a1b5ee894756a57af7a549f727729cd22def013
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-21 11:24:44 +0530

    v1.2

--- Nagios-code/log-http-error.sh
+++ Nagios-code/log-http-error.sh
@@ -1,5 +1,6 @@
 ubuntu-v2
 **********
+
  #!/bin/bash
 
 # mkdir /home/$(whoami)/log
@@ -38,9 +39,40 @@ else
 centos-v2
 *********
 
+ #!/bin/bash
+
+# mkdir /home/$(whoami)/log
+ touch /home/$(whoami)/log/data_after
+ touch /home/$(whoami)/log/data_before
+
+ sudo tail -1 /var/log/httpd/error_log > /home/$(whoami)/log/data_after
+ msg=$(tail -1 /home/$(whoami)/log/data_after)
+ error=$(tail -1 /home/$(whoami)/log/data_after | grep -w down | wc -l)
+ if cmp -s /home/$(whoami)/log/data_after /home/$(whoami)/log/data_before
+ then
+         {
+             echo "ok --> NO new logs"
+             exit 0
+         }
+else
+        {
+                if (( $error>=1 ));then
+                        {
+             echo "CRIRICAL-ERROR  --> $msg"
+             cp /home/$(whoami)/log/data_after /home/$(whoami)/log/data_before
+             exit 2
+                        }
+                else
+                        {
+             echo "WARNING --> $msg"
+             cp /home/$(whoami)/log/data_after /home/$(whoami)/log/data_before
+             exit 1
+                        }
+                fi
+        }
 
 
-
+ fi
 
 
 ##################################################################################################################################################################
