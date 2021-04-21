b10c4a6f99428945f2a93d5259e4f03269298ed1
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-21 10:58:26 +0530

    Update Log-error-centos&ubuntu.sh

--- Nagios-code/Log-error-centos&ubuntu.sh
+++ Nagios-code/Log-error-centos&ubuntu.sh
@@ -45,20 +45,46 @@ else
 centos-v2
 *********
 
+#!/bin/bash
+
+# mkdir /home/$(whoami)/log
+ touch /home/$(whoami)/log/data_after1
+ touch /home/$(whoami)/log/data_before1
+
+ sudo tail -1 /var/log/messages > /home/$(whoami)/log/data_after1
+ msg=$(tail -1 /home/$(whoami)/log/data_after1)
+ error=$(tail -1 /home/$(whoami)/log/data_after1 | grep -w  'Stopped' | wc -l)
+
+ if cmp -s /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
+ then
+         {
+             echo "ok --> NO new logs"
+             exit 0
+     }
+else
+        {
+
+        if (( $error>=1 ));then
+
+           {
+             echo "CRITICAL-ERROR --> $msg"
+             cp /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
+             exit 2
+     }
+
+    else
+            {
+
+             echo "WARNING --> $msg"
+             cp /home/$(whoami)/log/data_after1 /home/$(whoami)/log/data_before1
+             exit 1
+
+            }
+    fi
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+ fi
 
 ########################################################################################################################################################
 
