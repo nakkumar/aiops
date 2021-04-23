e07d2977c915b1514e37b61863eb6ef3531469dc
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-23 12:46:26 +0530

    v1.0

---
+++ Nagios-code/top5-io-rundeck-dummy.ps1
@@ -0,0 +1,226 @@
+NAME-RESTART
+*************
+
+gwmi Win32_PerfFormattedData_PerfProc_Process | sort IOReadBytesPersec -des | select  name -First 6  | out-file -filepath C:\first_io_name.txt
+
+$name1 = get-content C:\first_io_name.txt | Select-Object -Index 3  | where{$_ -ne ""}
+$name2 = get-content C:\first_io_name.txt | Select-Object -Index 4  | where{$_ -ne ""}
+
+$pattern = '[^a-zA-Z]'
+
+$name1 -replace $pattern, '' |  where{$_ -ne ""} | out-file -filepath C:\aaa1.txt
+$name2 -replace $pattern, '' |  where{$_ -ne ""} | out-file -filepath C:\aaa2.txt
+
+
+$total ="Total"
+
+$filter1 = get-content C:\aaa1.txt | Select-Object -Index 0  | where{$_ -ne ""}
+$filter2 = get-content C:\aaa2.txt | Select-Object -Index 0  | where{$_ -ne ""}
+
+Write-Output "$filter1"
+
+
+$check=0
+
+
+if ($filter1 -eq $total)
+
+{
+	Write-Output "EQUAL  $filter1-->$total"
+	
+	$value=get-service | select-object Name | out-file -filepath C:\topcpu.txt
+
+$count=(get-service | select-object Name).Length
+
+$count1=$count+1
+
+$val=2
+
+while($val -le $count1)
+     {
+      $val++
+      Write-Output  "$val"
+
+          $value2=Get-Content "C:\topcpu.txt" | Select-Object -Index $val
+
+          $space=$value2 -replace '\s',''
+
+          #Write-Output "$value2"
+
+          $value3=$space | select-string $filter2
+
+          if($value3)
+          {
+                  Write-Output "service#############$space#######restared"
+                  Restart-Service  $space
+                  
+				  $check=$check+1
+          }
+          else
+          {
+                  Write-Output "TWO NO #NAME# VALUES"
+          }
+
+          #Write-Output "$value3"
+     }
+		
+}
+else
+{
+	Write-Output "NOT EQUAL  #$filter1#$total#"
+	
+$value=get-service | select-object Name | out-file -filepath C:\topcpu.txt
+
+$count=(get-service | select-object Name).Length
+
+$count1=$count+1
+
+$val=2
+
+while($val -le $count1)
+     {
+      $val++
+      Write-Output  "$val"
+
+          $value2=Get-Content "C:\topcpu.txt" | Select-Object -Index $val
+
+          $space=$value2 -replace '\s',''
+
+          #Write-Output "$value2"
+
+          $value3=$space| select-string $filter1
+
+          if($value3)
+          {
+                  Write-Output "service#############$space#######restared"
+                  Restart-Service  $space
+                  
+				  $check=$check+1
+          }
+          else
+          {
+                  Write-Output "ONE NO #NAME# VALUE"
+          }
+
+          #Write-Output "$value3"
+     }
+	
+}
+
+PID-RESTART
+************
+gwmi Win32_PerfFormattedData_PerfProc_Process | sort IOReadBytesPersec -des | select  IDProcess -First 6  | out-file -filepath C:\first_io_pid.txt
+
+$pid1 = get-content C:\first_io_pid.txt | Select-Object -Index 3
+
+$check=0
+
+$num=0
+
+if ($pid1 -ge $num)
+{
+	
+Write-Output  "first ---> $pid1"
+
+################ service restart value1 #######################
+
+$value=get-service | select-object Name | out-file -filepath C:\topcpu.txt
+
+$count=(get-service | select-object Name).Length
+
+$count1=$count+1
+
+$val=2
+
+while($val -le $count1)
+     {
+      $val++
+      Write-Output  "$val"
+
+          $value2=Get-Content "C:\topcpu.txt" | Select-Object -Index $val
+
+          $space=$value2 -replace '\s',''
+
+          #Write-Output "$value2"
+
+          $value3=tasklist /svc /fi "SERVICES eq $space" | select-string $pid1
+
+          if($value3)
+          {
+                  Write-Output "service#############$space#######restared"
+                  Restart-Service  $space
+                  
+				  $check=$check+1
+          }
+          else
+          {
+                  Write-Output "ONE NO VALUE"
+          }
+
+          #Write-Output "$value3"
+     }
+
+
+
+
+}
+
+else
+
+{
+	
+$pid2 = get-content C:\first_io_pid.txt | Select-Object -Index 4
+Write-Output  "second --> $pid2"
+
+################ service restart value2 #######################
+
+$value=get-service | select-object Name | out-file -filepath C:\topcpu.txt
+
+$count=(get-service | select-object Name).Length
+
+$count1=$count+1
+
+$val=2
+
+while($val -le $count1)
+     {
+      $val++
+      Write-Output  "$val"
+
+          $value2=Get-Content "C:\topcpu.txt" | Select-Object -Index $val
+
+          $space=$value2 -replace '\s',''
+
+          #Write-Output "$value2"
+
+          $value3=tasklist /svc /fi "SERVICES eq $space" | select-string $pid2
+
+          if($value3)
+          {
+                  Write-Output "service#############$space#######restared"
+                  Restart-Service  $space
+                  $check=$check+1
+          }
+          else
+          {
+                  Write-Output "TWO NO VALUE"
+          }
+
+          #Write-Output "$value3"
+     }	
+	
+
+
+}
+
+if ($check -ge 1)
+
+{
+	Write-Output "SERVICE RESTARED"
+}
+
+else
+
+{
+	Write-Output "NO SERVICE RESTARED"
+}
