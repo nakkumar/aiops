1ea21294d64c1b00a878ae2ebb2a323965172691
arun <61176789+nakkumar@users.noreply.github.com>
2021-04-29 16:10:25 +0530

    Update Event-log-NEW.ps1

--- Nagios-code/Event-log-NEW.ps1
+++ Nagios-code/Event-log-NEW.ps1
@@ -29,3 +29,22 @@ else
 {
 	Write-Output "Files are the same"
 	}
+
+
+###############################  OLD  ##########################
+
+### we are monitoring the Application log for the below program
+$line = Get-WinEvent -LogName Application -MaxEvents 1
+
+$msg = $line.Message 
+
+if ($line.LevelDisplayName -eq "Error")
+{
+Write-Output "WARNING '$msg' "
+exit 1
+}
+else
+{
+Write-Output "ok '$msg' "
+exit 0
+}
